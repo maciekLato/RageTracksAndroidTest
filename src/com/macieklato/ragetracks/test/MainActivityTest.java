0519e15f678812a6a038b5e51b8c1747e44855e7
maciekLato <maciejplato@gmail.com>
2014-02-19 16:01:21 -0600

    mainactivity test updated

--- src/com/macieklato/ragetracks/test/MainActivityTest.java
+++ src/com/macieklato/ragetracks/test/MainActivityTest.java
@@ -1,5 +1,12 @@
 package com.macieklato.ragetracks.test;
+import com.macieklato.ragetracks.activity.MainActivity;
 
-public class MainActivityTest {
+import android.test.ActivityInstrumentationTestCase2;
+
+public class MainActivityTest extends
+		ActivityInstrumentationTestCase2<MainActivity> {
+		public MainActivityTest(){
+			super(MainActivity.class);
+		}
 
 }
